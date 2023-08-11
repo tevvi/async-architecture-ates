8573eba0f6d3cfa59499cd208d6548bc3d7e9bcd
astupak <Anton.Stupak@bia-tech.ru>
2023-08-12 00:22:59 +0300

    temporary implementation auth service

---
+++ auth/config/routes.rb
@@ -0,0 +1,10 @@
+require 'karafka/web'
+
+Rails.application.routes.draw do
+  post "/graphql", to: "graphql#execute"
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+  mount Karafka::Web::App, at: '/karafka'
+end
