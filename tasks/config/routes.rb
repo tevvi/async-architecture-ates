23e97c9d80f4e4b78e99cf2ff1d5f909db2605ea
astupak <Anton.Stupak@bia-tech.ru>
2023-08-13 01:49:20 +0300

    add tasks service

---
+++ tasks/config/routes.rb
@@ -0,0 +1,9 @@
+require 'karafka/web'
+
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+  mount Karafka::Web::App, at: '/karafka'
+end
